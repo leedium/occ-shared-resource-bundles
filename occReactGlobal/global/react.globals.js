705e72d68e59071e1c751faf92b3a2ee7c565b21
David Lee <developer@leedium.com>
2018-12-17 21:38:02 +0100

    set production back to default

--- occReactGlobal/global/react.globals.js
+++ occReactGlobal/global/react.globals.js
@@ -1,10 +1,8 @@
 // update this for dev / prod version
-
-// var REACT_PATH = "https://unpkg.com/react@16.6.3/umd/react.production.min";
-// var REACT_DOM =  "https://unpkg.com/react-dom@16.6.3/umd/react-dom.production.min";
-
-var REACT_PATH = "https://unpkg.com/react@16.6.3/umd/react.development";
-var REACT_DOM =  "https://unpkg.com/react-dom@16.6.3/umd/react-dom.development";
+var REACT_PATH = "https://unpkg.com/react@16.6.3/umd/react.production.min";
+var REACT_DOM =  "https://unpkg.com/react-dom@16.6.3/umd/react-dom.production.min";
+// var REACT_PATH = "https://unpkg.com/react@16.6.3/umd/react.development";
+// var REACT_DOM =  "https://unpkg.com/react-dom@16.6.3/umd/react-dom.development";
 requirejs.config({
   paths: {
     react: REACT_PATH,
