ba29d888481767651f0e1c758821a1665219e0f2
David Lee <developer@leedium.com>
2019-01-02 15:39:51 +0100

    latest bundle

--- occReactGlobal/global/react.globals.js
+++ occReactGlobal/global/react.globals.js
@@ -1,5 +1,5 @@
 // update this for dev / prod version
-var REACT_PATH = "https://unpkg.com/react@16.6.3/umd/react.production.min";
+var REACT_PATH = "https:g//unpkg.com/react@16.6.3/umd/react.production.min";
 var REACT_DOM =  "https://unpkg.com/react-dom@16.6.3/umd/react-dom.production.min";
 // var REACT_PATH = "https://unpkg.com/react@16.6.3/umd/react.development";
 // var REACT_DOM =  "https://unpkg.com/react-dom@16.6.3/umd/react-dom.development";
