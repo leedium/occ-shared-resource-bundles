e77db9c42aa6ab05d40a3569829fc61d56a550f5
David Lee <developer@leedium.com>
2019-01-02 19:26:06 +0100

    latest working without split chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,50 @@
 const path = require('path');
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const webpack = require('webpack');
 
 const config = {
     entry: {
-        "sharedBundles": [
-            'react',
-            'react-dom',
-            'styled-components'
+        "file": [
+            './app/index.js'
         ]
     },
+    // entry: {
+    //     "sharedBundles": [
+    //         'react',
+    //         'react-dom',
+    //         'styled-components'
+    //     ]
+    // },
     module: {
         rules: [
             {
-                test: /\.js|jsx?$/,
+                test: /\.js?$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
             },
         ]
     },
-    resolve: {
-        extensions: ['.js', '.jsx', '.json', '.less', '.css'],
-        modules: [__dirname, 'node_modules']
-    },
     output: {
         path: path.resolve(__dirname, './vendorBundle/global'),
         filename: "[name].dll.js",
-        library: `/file/globals/z4ma.globals.min.js`,
+        library: "/file/globals/z4ma.globals.min.js",
         libraryTarget: "amd"
     },
     plugins: [
+        new BundleAnalyzerPlugin(),
         new webpack.DllPlugin({
             name: '[name]',
             path: './build/library/[name].json'
         })
-    ]
-    // plugins:[
-    //     new webpack.HashedModuleIdsPlugin()
-    // ],
-    // optimization: {
-    //     splitChunks: {
-    //         chunks: 'all',
-    //         maxInitialRequests: Infinity,
-    //         minSize: 0,
-    //         minChunks: 10,
-    //         name: '/file/globals/z4ma.globals.min.js',
-    //         cacheGroups: {
-    //             sharedBundles: {
-    //                 // test: /[\\/]node_modules[\\/]/,
-    //                 test: /[\\/]node_modules[\\/](react|react-dom|styled-components)[\\/]/,
-    //                 // name(module) {
-    //                 //     const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-    //                 //     return `npm.${packageName.replace('@','')}`;
-    //                 // }
-    //             }
-    //         }
-    //     }
-    // }
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            minChunks: 10
+        }
+    }
 };
 
 module.exports = config;
