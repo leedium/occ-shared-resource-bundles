c737518dd56558cbe08ed05eef0148be9184dc74
David Lee <developer@leedium.com>
2019-01-03 14:19:29 +0100

    complete refactor to bundled chunks via webpack SplitChunk plugin, renamed repo, updated readme, added linting and code styling features.

--- app/index.js
+++
@@ -1,7 +0,0 @@
-// import 'react-hot-loader';
-// import 'react-hot-loader/root';
-// import 'stylis/stylis.min'
-// import 'stylis-rule-sheet'
-// import 'react';
-// import 'react-dom';
-// import 'styled-components';
--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,89 @@
-const path = require('path');
-const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer");
-const webpack = require('webpack');
+/*
+ * Copyright (c) 2018 LEEDIUM.
+ * This file is subject to the terms and conditions
+ * defined in file 'LICENSE.txt', which is part of this
+ * source code package.
+ */
 
-const config = {
-    entry: {
-        "vendor": [
-            // './app/index.js'
-            // 'react',
-            // 'react-hot-loader',
-            // 'react-dom',
-            'styled-components',
-        ]
-    },
-    // devtool: 'eval-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    },
-    resolve: {
-        modules: ["node_modules"],
-        extensions: [".js", ".json"],
-        alias: {
-            'styled-components': path.resolve(__dirname,'node_modules', 'styled-components')
-        }
-    },
-    stats: {
-        // all: true,
-        colors: true,
-        reasons: true,
-        chunks: true,
-        // moduleTrace: true
-    },
-    output: {
-        path: path.resolve(__dirname, './vendorBundle/global'),
-        filename: "[name].dll.js",
-        library: "/file/globals/z4ma.globals.min.js",
-        libraryTarget: "amd"
-    },
-    // externals: {
-    //     "styled-components": {
-    //         commonjs: "styled-components",
-    //         commonjs2: "styled-components",
-    //         amd: "styled-components",
-    //     }
-    // },
-    plugins: [
-        new BundleAnalyzerPlugin(),
-        new webpack.DllPlugin({
-            name: '[name]',
-            path: './build/library/[name].json'
-        })
-    ],
-    optimization: {
-        // runtimeChunk:{
-        //     name:"sharedBundles"
-        // },
-        minimize: false,
-        splitChunks: {
-            chunks: 'all',
-            maxInitialRequests: Infinity,
-            minSize: 0,
-            minChunks: 50,
-            // cacheGroups: {
-            //     sharedBundles: {
-            //         test: /[\\/]node_modules[\\/]/,
-            //         name(module) {
-            //             const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            //             return `npm.${packageName.replace('@','')}`;
-            //         }
-            //     }
-            // }
-        },
+/**
+ * @project occ-react-component
+ * @file webpack.config.js
+ * @company leedium
+ * @createdBy davidlee
+ * @contact david@leedium.com
+ * @dateCreated 03/01/2019
+ * @description  This configuration file splits modules defined by you into a bundle
+ * that your occ-react-components can use as dependencies
+ * https://github.com/leedium/occ-react-component
+ *
+ *
+ */
 
+const path = require("path");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const webpack = require("webpack");
+
+const OCC_GLOBAL_FILE_NAME = "z4ma.globals.min.js";
+
+module.exports = (env, argv) => ({
+  entry: {
+    vendor: ["react", "react-hot-loader", "react-dom", "styled-components"]
+  },
+  devtool: argv.mode === "production" ? "none" : "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js", ".json"],
+    alias: {
+      "styled-components": path.resolve(
+        __dirname,
+        "node_modules",
+        "styled-components"
+      )
     }
-};
-
-module.exports = config;
-
-
-
+  },
+  stats: {
+    colors: true,
+    reasons: false,
+    chunks: true
+  },
+  output: {
+    path: path.resolve(__dirname, "./vendorDLL"),
+    filename: "[name].dll.js",
+    library: `/file/globals/${OCC_GLOBAL_FILE_NAME}`,
+    libraryTarget: "amd"
+  },
+  plugins: [
+    // new BundleAnalyzerPlugin(),
+    new webpack.DllPlugin({
+      name: "[name]",
+      path: "./vendorManifest/[name].json"
+    })
+  ],
+  optimization: {
+    minimize: false,
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      minChunks: 50
+      // cacheGroups: {
+      //     sharedBundles: {
+      //         test: /[\\/]node_modules[\\/]/,
+      //         name(module) {
+      //             const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+      //             return `npm.${packageName.replace('@','')}`;
+      //         }
+      //     }
+      // }
+    }
+  }
+});
